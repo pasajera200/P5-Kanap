df9528872a8fe7d3a219aa946ecef102cdc2d8f3
Paola <apaola1030@gmail.com>
2022-10-26 22:37:10 +0200

    prix récuperé avec fetch

--- front/js/cart.js
+++ front/js/cart.js
@@ -13,49 +13,59 @@ function getItemsFromCache() {
 
       let addKanap = itemObject;
 
-      const getPrice = () => {
-        fetch(`http://localhost:3000/api/products/${addKanap.id}`)
-          .then((response) => response.json())
-          .then((produits) => {
-            price = produits.price;
-            addKanap.price = price;
-            console.log(price);
-          });
-      };
-      
-      getPrice();
+      // const getPrice = () => {
+      //   fetch(`http://localhost:3000/api/products/${addKanap.id}`)
+      //     .then((response) => response.json())
+      //     .then((produits) => {
 
-      panierItems();
+      //       price = produits.price;
+      //       console.log(produits.price);
+      //     });
+      // };
+      // getPrice();
+    }
+  }
+}
 
-      function panierItems() {
-        let cartItems = document.getElementById('cart__items');
-        for (const produits of cart) {
-          const article = createArticle(produits);
-          cartItems.appendChild(article);
-          article.addEventListener('click', cartItems);
-        }
-      }
+panierItems();
 
-      function createArticle(produits) {
-        const article = document.createElement('article');
-        article.classList.add('cart__item');
-        article.setAttribute = ('data-id', produits._id);
-        article.setAttribute = ('data-color', produits.color);
-        article.setAttribute = 'data';
-        document.querySelector('.cart__item__content__description');
-        article.innerHTML = `<div class="cart__item__img">
-    <img src="${produits.imageUrl}" alt="Photographie d'un canapé">
+function panierItems() {
+  let cartItems = document.getElementById('cart__items');
+  for (const sofa of cart) {
+    fetch(`http://localhost:3000/api/products/${sofa.id}`)
+      .then((response) => response.json())
+      .then((produits) => {
+        console.log(produits.price);
+        const article = createArticle(sofa, produits.price);
+        cartItems.appendChild(article);
+      });
+
+    // article.addEventListener('click', cartItems);
+    // const p = document.createElement('p');
+    // p.textContent = sofa.price + '€';
+  }
+}
+
+function createArticle(sofa, price) {
+  const article = document.createElement('article');
+  article.classList.add('cart__item');
+  article.setAttribute = ('data-id', sofa._id);
+  article.setAttribute = ('data-color', sofa.color);
+  article.setAttribute = 'data';
+  document.querySelector('.cart__item__content__description');
+  article.innerHTML = `<div class="cart__item__img">
+    <img src="${sofa.imageUrl}" alt="Photographie d'un canapé">
   </div>
   <div class="cart__item__content">
     <div class="cart__item__content__description">
-      <h2>${produits.name}</h2>
-      <p>${produits.color}</p>
-      <p>${produits.price + '€'}</p>
+      <h2>${sofa.name}</h2>
+      <p>${sofa.color}</p>
+      <p>${price + '€'}</p>
     <div class="cart__item__content__settings">
       <div class="cart__item__content__settings__quantity">
         <p>Qté : </p>
         <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${
-          produits.quantity
+          sofa.quantity
         }>
       </div>
       <div class="cart__item__content__settings__delete">
@@ -63,10 +73,5 @@ function getItemsFromCache() {
       </div>
     </div>
   </div>`;
-        return article;
-      }
-
-      getPrice();
-    }
-  }
+  return article;
 }
