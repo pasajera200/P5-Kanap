216b09dfcf3db472affe1981838c018b97be50c6
Paola <apaola1030@gmail.com>
2022-11-07 10:22:25 +0100

    nouvelle function updatePrixEtquantity

--- front/js/cart.js
+++ front/js/cart.js
@@ -17,30 +17,28 @@ function getItemsFromCache() {
 panierItems();
 
 function panierItems() {
-  let panierItems = document.getElementById('cart__items');
+  const panierItems = document.getElementById('cart__items');
   for (const sofa of panier) {
     fetch(`http://localhost:3000/api/products/${sofa.id}`)
       .then((response) => response.json())
       .then((produits) => {
         const article = createArticle(sofa, produits.price);
         panierItems.appendChild(article);
-        deleteItem(article, sofa);
-        afficherItemQuantity(sofa);
-        afficherTotalQuantity(sofa);
         listenQuantitychange(article, sofa, panier);
-        updateTotalQuantity(panier);
-        // updateTotalPrice(panier);
+        deleteItem(article); 
       });
   }
   return panier;
 }
 
-function createArticle(sofa, price) {
-  updateTotalPrice(price, panier);
+function createArticle(sofa, price, item) {
+  displayTotalPrice(item, price, panier,)
+  displayTotalQuantity()
   const article = document.createElement('article');
   article.classList.add('cart__item');
   article.setAttribute = ('data-id', sofa._id);
   article.setAttribute = ('data-color', sofa.color);
+ 
   article.innerHTML = `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}"><div class="cart__item__img">
     <img src="${sofa.imageUrl}" alt="Photographie d'un canapé">
   </div>
@@ -68,68 +66,49 @@ function createArticle(sofa, price) {
   return article;
 }
 
-function deleteItem(article, sofa, panier, price) {
+function deleteItem(article) {
   const buttonSupprimer = article.querySelector('.deleteItem');
   buttonSupprimer.addEventListener('click', (e) => {
-    console.log(e.target.id);
     localStorage.removeItem(e.target.id);
     window.location.reload(true);
-    // sofa.quantity = 0;
-    // updateTotalPrice(sofa, price, panier);
-    // updateTotalQuantity(sofa);
-    // article.remove();
-    // const key = `product-${id}-${color}`;
-    // console.log(key);
-    // localStorage.getItem(key);
   });
 }
 
-function afficherItemQuantity(sofa) {
-  const quantityInput = document.querySelector('input[name="itemQuantity"]');
-  // quantityInput.value = sofa.quantity;
+
+function UpdatePrixEtquantity(id, newValue, item) {
+  console.log(id)
+  const itemToUpdate = panier.find(item => item.id === id)
+  console.log("newValue", newValue)
+  itemToUpdate.quantity = newValue
+  console.log(panier)
+  displayTotalPrice(item, price, panier)
+  displayTotalQuantity()
 }
 
-function updateTotalPrice(price, panier) {
-  let total = 0;
-  const totalPrice = document.querySelector('#totalPrice');
-  panier.forEach((sofa) => {
-    const totalUnitPrice = price * sofa.quantity;
-    total += totalUnitPrice;
-  });
-  // console.log(total);
-  totalPrice.textContent = total;
+function displayTotalPrice( item, price, panier) {
+  const totalPrice = document.querySelector("#totalPrice")
+  const total = panier.reduce((total, item) => total = price * item.quantity, 0)
+  console.log(total)
+    totalPrice.textContent = total
+  }
+
+function displayTotalQuantity() {
+  const totalQuantity = document.querySelector("#totalQuantity")
+  const total = panier.reduce((total, item) => total + item.quantity, 0); 
+  totalQuantity.textContent = total; 
 }
 
-function updateTotalQuantity(panier) {
-  let totalQuantity = 0;
-  panier.forEach((sofa) => {
-    totalQuantity += sofa.quantity;
-  });
-  afficherTotalQuantity(totalQuantity);
-}
 
-function afficherTotalQuantity(total) {
-  const totalQuantity = document.getElementById('totalQuantity');
-  totalQuantity.innerHTML = total;
-}
 
-function listenQuantitychange(article, sofa, panier, price) {
+function listenQuantitychange(article, panier, price) {
   const itemQuantity = article.querySelector('.itemQuantity');
   itemQuantity.addEventListener('change', (e) => {
     let data = JSON.parse(localStorage.getItem(e.target.id));
     data.quantity = parseInt(e.target.value);
-
     localStorage.setItem(e.target.id, JSON.stringify(data));
-
     window.location.reload(true);
-
-    console.log(e.target.value);
-    console.log(data);
-    // console.log(itemQuantity.id);
-    sofa.quantity = e.target.value;
-    afficherItemQuantity(sofa);
-    updateTotalPrice(price);
-    updateTotalQuantity(panier);
-    afficherTotalQuantity(panier);
+    UpdatePrixEtquantity(data.id, data.quantity) 
+    displayTotalQuantity()
+    displayTotalPrice(item, price, panier,)
   });
 }
